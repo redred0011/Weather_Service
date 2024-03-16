c48fab6cb07ddf61978f8b01d7bf62ae6edf4821
Bartek_Anczewski <s094217@student.tu.kielce.pl>
2024-03-16 14:02:21 +0100

    implementation of sending emails

--- src/main/java/org/example/world_windsufers/WorldWindsufersWeatherServiceApplication.java
+++ src/main/java/org/example/world_windsufers/WorldWindsufersWeatherServiceApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @SpringBootApplication
 @EnableConfigurationProperties(WeatherProperties.class)
 @EnableFeignClients
 @EnableCaching
+@EnableAsync
 public class WorldWindsufersWeatherServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/org/example/world_windsufers/controller/WeatherController.java
+++ src/main/java/org/example/world_windsufers/controller/WeatherController.java
@@ -5,6 +5,7 @@ import org.example.world_windsufers.model.Weather;
 import org.example.world_windsufers.service.WeatherService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,8 +23,8 @@ public class WeatherController {
         return weatherService.getForecastsForAllDestinations();
     }
     @GetMapping("/best/forecasts")
-    public Weather getBestForecastForWindsurfing() {
-        Optional<Weather> bestForecast = weatherService.findBestWindsurfingLocation();
+    public Weather getBestForecastForWindsurfing(@RequestParam(required = false)String email) {
+        Optional<Weather> bestForecast = weatherService.findBestWindsurfingLocation(email);
         return bestForecast.orElseThrow(() -> new RuntimeException("Not found the best localization for Wind Surfing...."));
     }
 
